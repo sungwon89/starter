70adea03412c5d494731338297ae40eaffb53f77
sungwon89 <pinkcleo1@gmail.com>
2017-03-24 13:33:22 +0900

    6th Commit

--- helloworld_make.py
+++ helloworld_make.py
@@ -5,8 +5,8 @@ HOMEDIR="/home/user/"
 FILENAME_FILESYSTEM="collect_test_filesystem.csv"
 FILENAME_SYSLOG="collect_test_syslog.csv"
 HOSTNAME=platform.uname()[1]
-CLIENT_SYSLOG_FILE_LIST=['/home/user/ubuntu1_test_syslog.csv', '/home/user/ubuntu2_test_syslog.csv']
-CLIENT_USED_FILE_LIST=['/home/user/ubuntu1_test_filesystem.csv', '/home/user/ubuntu2_test_filesystem.csv']
+CLIENT_SYSLOG_FILE_LIST=['/home/user/ubuntu1_test_syslog.csv','/home/user/ubuntu2_test_syslog.csv']
+CLIENT_USED_FILE_LIST=['/home/user/ubuntu1_test_filesystem.csv','/home/user/ubuntu2_test_filesystem.csv']
 
 
 def SyslogCollector():
--- helloworld_make2.py
+++ helloworld_make2.py
@@ -9,6 +9,35 @@ CLIENT_USED_FILE_LIST=['/home/user/ubuntu1_test_filesystem.csv', '/home/user/ubu
 
 
 def SyslogCollector():
+    SyslogOutput = open(FILENAME_SYSLOG, 'w')
+    fieldnames = ['Hostname', 'Event']
+    SyslogWriter = csv.DictWriter(SyslogOutput, fieldnames=fieldnames)
+    SyslogWriter.writerow(dict(zip(fieldnames, fieldnames)))
+    for list in CLIENT_SYSLOG_FILE_LIST:
+        file = open(list,'r')
+        reader = csv.DictReader(file)
+        syslog_list = []
+        for row in reader:
+            syslog_list.append(row)
+        file.close()
+        SyslogWriter.writerows(syslog_list)
+    SyslogOutput.close()
 
+def FilesystemUsedCollector():
+    UsedOutput = open(FILENAME_FILESYSTEM, 'w')
+    fieldnames = ['Hostname','Filesystem','Used%']
+    UsedWriter = csv.DictWriter(UsedOutput, fieldnames=fieldnames)
+    UsedWriter.writerow(dict(zip(fieldnames, fieldnames)))
+    for list in CLIENT_USED_FILE_LIST:
+        file = open(list,'r')
+        reader = csv.DictReader(file)
+        used_list = []
+        for row in reader:
+            used_list.append(row)
+        file.close()
+        #print used_list
+        UsedWriter.writerows(used_list)
+    UsedOutput.close()
 
 SyslogCollector()
+FilesystemUsedCollector()
