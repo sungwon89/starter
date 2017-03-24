8da98db4275ed149ada7b72a768cf55f36241284
sungwon89 <pinkcleo1@gmail.com>
2017-03-24 14:17:50 +0900

    6th Commit

--- helloworld_make2.py
+++ helloworld_make2.py
@@ -7,6 +7,7 @@ CLIENT_SYSLOG_FILE_LIST=['/home/user/ubuntu1_test_syslog.csv', '/home/user/ubunt
 CLIENT_USED_FILE_LIST=['/home/user/ubuntu1_test_filesystem.csv', '/home/user/ubuntu2_test_filesystem.csv','/home/user/ubuntu3_test_filesystem.csv']
 
 
+
 def SyslogCollector():
     SyslogOutput = open(FILENAME_SYSLOG, 'w')
     Fieldname = ['Hostname', 'Event']
